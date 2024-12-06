1ece5b026ee1dee3a887ae44ad8dbc86fc7b3d10
andrii <rozrutskiy@gmail.com>
2024-12-06 21:34:38 +1100

    refactor main.py to improve code formatting and maintainability

--- main.py
+++ main.py
@@ -6,8 +6,8 @@ if __name__ == "__main__":
     port = int(os.environ.get("PORT", 8000))
 
     uvicorn.run(
-        app="app.main:app",  # Points to the app instance inside 'app.main'
-        host="0.0.0.0",      # Use '0.0.0.0' to allow external access
-        port=port,           # Use the dynamic port from the environment variable
-        reload=True          # Enable auto-reload during development (can be removed in production)
+        "app.main:app",  # Points to the app instance inside 'app.main'
+        host="0.0.0.0",  # Use '0.0.0.0' to allow external access
+        port=port,       # Use the dynamic port from the environment variable
+        reload=True      # Enable auto-reload during development (can be removed in production)
     )
